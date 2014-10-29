6b5c80ef99c0e25a567a3456aa32f2d0f5076898
robinlan <robinlan@users.noreply.github.com>
2014-10-29 11:58:29 +0800

    Update RandomNum.r
    a little modification

--- RandomNum.r
+++ RandomNum.r
@@ -1,7 +1,7 @@
 RandomNum<-function(row,col,method='Sampling',replace='T',max_bound=100){
- w<-matrix(0,row,col,replace='T')
+ w<-matrix(0,row,col)
  if(method=='Sampling'){
-  ss<-sample(1:max_bound,col*row)
+  ss<-sample(1:max_bound,col*row,replace='T')
   for(a in 1:row){
    for(b in 1:col){
     w[a,b]<-ss[b*row+a-row]
