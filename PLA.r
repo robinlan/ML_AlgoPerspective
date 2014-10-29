f68d6a0463584bd7ce6c0fdc44da359863e31861
robinlan <robinlan@users.noreply.github.com>
2014-10-29 12:11:07 +0800

    Update PLA.r

--- PLA.r
+++ PLA.r
@@ -1,1 +1,5 @@
-##A function to implement Perceptron Learning Algorithm(PLA)
+##A function to implement Perceptron Learning Algorithm(PLA) in R programing language
+
+##Step 1: Generate random samples of weight(w)
+source("Common/RandomNum.r")
+RandomNum(5,6)
